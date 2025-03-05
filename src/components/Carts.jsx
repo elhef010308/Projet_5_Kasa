aaa801258f07ad2718fd9f9c6a430abaedf548d2
Elhef_2038 <lfougerard@gmail.com>
2025-03-05 15:26:50 +0100

    Génération des cartes avec MAP (mise en page générale effectuée) : cartes pas encore terminées !

---
+++ src/components/Carts.jsx
@@ -0,0 +1,11 @@
+function Carts({ title }) {
+    return (
+        <div className="cart-location">
+            <p>{title}</p>
+        </div>
+    );
+}
+
+export default Carts;
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react';
+import Carts from "../components/Carts";
+
 function Home() {
-    return <h1>Chez vous, partout et ailleurs</h1>;
+    const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/logements.json");
+                const data = await response.json();
+                setLocations(data);    // mettre à jour le state avec les logementds récupérés
+            } catch (error) {
+                console.error("Erreur de chargement : ", error);
+            }
+        };
+
+        fetchData();  // appeler la fonction asynchrone
+    }, []);
+
+    return (
+        <div className='cart-component'>
+            {locations.map((logement) => (
+                <Carts key={logement.id} title={logement.title} />
+            ))}
+        </div>
+    );
 }
 
 export default Home;
