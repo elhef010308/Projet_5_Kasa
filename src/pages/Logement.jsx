1223f80ccecde0ff1ab2aa3a07b0981f64919307
Elhef_2038 <lfougerard@gmail.com>
2025-03-07 14:57:14 +0100

    ETAPE 7 réalisée (restera la responsivité à faire)

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -7,6 +7,7 @@ import { Collapse } from "../components/Components";
 function Logement() {
     const { id } = useParams();
     const [location, setLocation] = useState(null);
+    const [currentPicture, setCurrentPicture] = useState(0);  // index de l'image affichée actuellement
 
     useEffect(() => {
         const fetchLocation = async () => {
@@ -23,24 +24,30 @@ function Logement() {
         fetchLocation();
     }, [id]);
 
-    if (!location) {
-        return <div>Chargement...</div>;
-    }
+    if (!location) return <div>Chargement...</div>;
+    
+
+    const previousImage = () => {
+        setCurrentPicture(prev => (prev === 0 ? location.pictures.length - 1 : prev - 1))
+    }   // si PREV = la première image (index = 0) alors on revient à la dernière, sinon on fait index - 1
+
+    const nextImage = () => {
+        setCurrentPicture((currentPicture + 1) % location.pictures.length)
+    }                                         // "&" ===> une fois à la dernière image, on retourne à la première 
 
     return (
         <div className="location-page">
-            <div className="location-image">
-                <img alt="vector-left" src={vectorLeft} />
-                <div className="image">
-                    <img alt={location.title} src={location.pictures[0]} />
-                    <p>1/x</p>
+            <div className="location-image" style={{ backgroundImage: `url(${location.pictures[currentPicture]})` }}>            
+                <div>
+                    <img alt="vector-left" src={vectorLeft} onClick={nextImage} />
+                    <img alt="vector-right" src={vectorRight} onClick={previousImage} />
                 </div>
-                <img alt="vector-right" src={vectorRight} />
+                <p>{currentPicture + 1}/{location.pictures.length}</p>
             </div>
 
             <div className="location-title">
                 <div className="title">
-                    <p>{location.title}</p>
+                    <h2>{location.title}</h2>
                     <p>{location.location}</p>
                 </div>
                 <div className="author">
@@ -50,30 +57,30 @@ function Logement() {
             </div>
 
             <div className="location-filter">                {/* MAP crée un bouton pour chaque tag du tableau location.tags */}
-                <div className="button-filter">              {/* key={index} permet à React d'identifier chaque bouton de façon unique */}           
+                <div className="button-filter">              {/* key={index} permet à React d'identifier chaque bouton de façon unique */}
                     {location.tags.map((tag, index) => (
-                        <button key={index}>{tag}</button>   
+                        <button key={index}>{tag}</button>
                     ))}                                      {/* {tag} = le contenu texte du bouton */}
                 </div>
                 <div className="stars">                      {/* Array(5) = créer un tableau de 5 éléments */}
-                    {[...Array(5)].map((_, index) => (       
-                        <span key={index}>{index < parseInt(location.rating) ? "★" : "☆"}</span>
+                    {[...Array(5)].map((_, index) => (
+                        <p key={index}>{index < parseInt(location.rating) ? "★" : "☆"}</p>
                     ))}                                      {/* MAP = parcoourir ce tableau */}
                 </div>                                       {/* A chaque itération, on vérifie si l'index est inf ou sup à la note du logement */}
             </div>                                           {/* Si la note de {location.rating} est SUP on affiche une étoile pleine, sinon une étoile vide */}
 
             <div className="location-collapse">
-                <Collapse title="Description" content={location.description} />
+                <Collapse title="Description" /*content={location.description}*/ />
                 <Collapse
                     title="Équipements"
-                    content={(
-                        <ul>          
+                    /*content={(
+                        <ul>
                             {location.equipments.map((equip, index) => (
                                 <li key={index}>{equip}</li>
                             ))}
-                        </ul>  
-                    )}          {/* {location.equipement} = tableau avec une liste d'équipement */}
-                />              {/* pour chaque élément (equip) on crée une <li> dynamiquement */}
+                        </ul>
+                    )}*/
+                />
             </div>
         </div>
     );
